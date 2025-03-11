ac5f605838dcea5f632a3a29cb6b2c4e712f3094
Leia Chang <leia.chang@exploratorium.edu>
2025-03-11 12:13:29 -0700

    multiple video sequences

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const WebSocket = require("ws");
 const wss = new WebSocket.Server({ port: 8080 });
 
-let currentVideo = "/videos/blink2.mp4"; // Default video
+// let currentVideo = "/videos/blink2.mp4"; // Default video
 
 wss.on("connection", (ws) => {
-    ws.send(currentVideo); // Send the correct file path
+    // ws.send(currentVideo); // Send the correct file path
 
     ws.on("message", (message) => {
-        currentVideo = message.toString();
-        console.log(currentVideo);
+        // currentVideo = message.toString();
+        console.log(JSON.parse(message));
         wss.clients.forEach(client => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(currentVideo); // Send the full path
+                client.send(message.toString()); 
             }
         });
     });
